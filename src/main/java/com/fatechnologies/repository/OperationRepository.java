635797cb30e7b6bf27de67ed58d07382c684f913
fabrice Assagou <fabrice.assagou@cinetpay.com>
2024-01-04 12:07:41 +0000

    feat: ajout des bénéfices

--- src/main/java/com/fatechnologies/repository/OperationRepository.java
+++ src/main/java/com/fatechnologies/repository/OperationRepository.java
@@ -2,7 +2,6 @@ package com.fatechnologies.repository;
 
 import com.fatechnologies.domaine.dto.TypeOperation;
 import com.fatechnologies.domaine.entity.OperationEntity;
-import com.fatechnologies.domaine.entity.TransactionEntity;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
